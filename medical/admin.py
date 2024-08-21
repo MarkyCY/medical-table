e0b29ceafd40a0ddc2a7395601990897745eb3ef
Marcos J. Cardenas P. <oranmarcos@gmail.com>
2024-08-21 02:50:51 -0400

    Add admin crud

--- medical/admin.py
+++ medical/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Doctor, Patient, Appointment, Specialization
+
+admin.site.register(Doctor)
+admin.site.register(Patient)
+admin.site.register(Appointment)
+admin.site.register(Specialization)
