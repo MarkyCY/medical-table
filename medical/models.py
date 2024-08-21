b61fd892215630110132c82f3c9eadcf9afac5a8
Marcos J. Cardenas P. <oranmarcos@gmail.com>
2024-08-21 02:51:18 -0400

    Upgrade to new Models Structure

--- medical/models.py
+++ medical/models.py
@@ -1,40 +1,6 @@
 from django.db import models
 
-class Doctors(models.Model):
-    id = models.AutoField(primary_key=True)
-    name = models.CharField(max_length=100)
-    specialization = models.CharField(max_length=100)
-    schedule = models.CharField(max_length=100)
-    contact = models.CharField(max_length=100)
-
-    def __str__(self):
-        return self.name
-
-class Patients(models.Model):
-    id = models.AutoField(primary_key=True)
-    name = models.CharField(max_length=100)
-    email = models.EmailField(unique=True)
-    phone = models.CharField(max_length=15)
-
-    def __str__(self):
-        return self.name
-
-class Appointments(models.Model):
-    STATUS_CHOICES = [
-        ('confirmed', 'Confirmada'),
-        ('canceled', 'Cancelada'),
-    ]
-
-    id = models.AutoField(primary_key=True)
-    doctor = models.ForeignKey(Doctors, on_delete=models.CASCADE)
-    patient = models.ForeignKey(Patients, on_delete=models.CASCADE)
-    date_time = models.DateTimeField()
-    status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='confirmed')
-
-    def __str__(self):
-        return f'{self.doctor.name} - {self.patient.name} - {self.date_time}'
-
-class Specializations(models.Model):
+class Specialization(models.Model):
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=100)
     description = models.TextField()
@@ -42,17 +8,38 @@ class Specializations(models.Model):
     def __str__(self):
         return self.name
 
-class Users(models.Model):
-    ROLE_CHOICES = [
-        ('admin', 'Administrador'),
-        ('patient', 'Paciente'),
-    ]
-
+class Doctor(models.Model):
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=100)
-    email = models.EmailField(unique=True)
-    password = models.CharField(max_length=100)
-    role = models.CharField(max_length=10, choices=ROLE_CHOICES)
+    specialization = models.ForeignKey(Specialization, on_delete=models.CASCADE)
+    schedule = models.JSONField()
+    contact = models.CharField(max_length=100)
 
     def __str__(self):
         return self.name
+    
+
+class Patient(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+    phone = models.CharField(max_length=15)
+    password = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+class Appointment(models.Model):
+    STATUS_CHOICES = [
+        ('confirmed', 'Confirmada'),
+        ('canceled', 'Cancelada'),
+    ]
+
+    id = models.AutoField(primary_key=True)
+    doctor = models.ForeignKey(Doctor, on_delete=models.CASCADE)
+    patient = models.ForeignKey(Patient, on_delete=models.CASCADE)
+    date_time = models.DateTimeField()
+    status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='confirmed')
+
+    def __str__(self):
+        return f'{self.doctor.name} - {self.patient.name} - {self.date_time}'
